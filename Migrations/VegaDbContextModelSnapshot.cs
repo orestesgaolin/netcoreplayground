12d4336f02da1338999440e06984ef611e1452a0
Dominik Roszkowski <kontakt@dominikroszkowski.pl>
2017-08-10 23:53:09 +0200

    Add new migrations

---
+++ Migrations/VegaDbContextModelSnapshot.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using netcoreplayground.Persistence;
+
+namespace Vega.Migrations
+{
+    [DbContext(typeof(VegaDbContext))]
+    partial class VegaDbContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("netcoreplayground.Models.Make", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Makes");
+                });
+
+            modelBuilder.Entity("netcoreplayground.Models.Model", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("MakeId");
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("MakeId");
+
+                    b.ToTable("Model");
+                });
+
+            modelBuilder.Entity("netcoreplayground.Models.Model", b =>
+                {
+                    b.HasOne("netcoreplayground.Models.Make", "Make")
+                        .WithMany("Models")
+                        .HasForeignKey("MakeId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
--- README.md
+++ README.md
@@ -15,16 +15,41 @@ Install [generator-aspnetcore-spa](https://www.npmjs.com/package/generator-aspne
 
 and then
 
-> dotnet restore
+`dotnet restore`
 
 Since this moment use:
 
-> dotnet watch run
+`dotnet watch run`
 
 ## Entity Framework Core
 Add `Microsoft.EntityFrameworkCore.Tools.DotNet` reference to .csproj which enables EF commands:
 
-> dotnet ef
-> dotnet add package Microsoft.EntityFrameworkCore.Design
-> dotnet ef migrations add InitialModel
-> dotnet ef migrations remove
+`dotnet ef`
+
+`dotnet add package Microsoft.EntityFrameworkCore.Design`
+
+`dotnet ef migrations add InitialModel`
+
+`dotnet ef migrations remove`
+
+## Troubleshooting db connection
+To use HeidiSQL to connect to database Vega use:
+
+```
+cd 'C:\Program Files\Microsoft SQL Server\130\LocalDB\Binn'
+SqlLocalDB.exe info
+if not running
+SqlLocalDB.exe start MSSQLLocalDb
+SqlLocalDB.exe info MSSQLLocalDb
+
+Name:               mssqllocaldb
+Version:            13.0.1601.5
+Shared name:
+Owner:              DESKTOP-DNALHJH\domin
+Auto-create:        Yes
+State:              Running
+Last start time:    10.08.2017 23:45:48
+Instance pipe name: np:\\.\pipe\LOCALDB#1D00D124\tsql\query
+```
+
+Then in connection manager use `\\.\pipe\LOCALDB#1D00D124\tsql\query` as a host name for _MS SQL Server (named pipe)_ connection
