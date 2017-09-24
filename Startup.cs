be075f63ba963c1e7ef69f2bb6788f01cd5aa1eb
Dominik Roszkowski <kontakt@dominikroszkowski.pl>
2017-09-24 20:40:00 +0200

    Register AutoMapper in startup

--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 using netcoreplayground.Persistence;
+using AutoMapper;
 
 namespace WebApplicationBasic
 {
@@ -30,6 +31,8 @@ namespace WebApplicationBasic
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAutoMapper();
+
             services.AddDbContext<VegaDbContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:Default"]));
             // Add framework services.
             services.AddMvc();
